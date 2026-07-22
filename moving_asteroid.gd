558f47ef6745a585600dd4cdfc091d79cdbbaf0d
Roxy Roocky <277807885+roxy-roocky@users.noreply.github.com>
2026-07-22 16:18:29 +0200

    👩‍💻 Add Godot project embedding the addon

---
+++ moving_asteroid.gd
@@ -0,0 +1,16 @@
+@tool
+extends Node2D
+
+@export var origin: Vector2
+
+@export_tool_button("Set Origin", "KeyPosition") var btn_origin = func():
+	origin = global_position
+	
+@export_tool_button("Reset", "UndoRedo") var btn_reset = func():
+	global_position = origin
+	
+@export var move:= false
+	
+func _process(delta: float) -> void:
+	if move:
+		global_position += $RoxyVector2D.direction * delta
